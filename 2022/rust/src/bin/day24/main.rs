6d6a5c2dc0d1c71232283cf88fb86f0b8e5fc04a
Matthew Hill <matthewchill7@gmail.com>
2022-12-24 23:14:10 -0500

    2022 Day 24 Improvement
    Make several improvements, including tracking only blizzards that
    you could possibly hit in each cycle, and switching from standard
    BFS to A*. Took the runtime for both parts down from several
    minutes to ~5 seconds.

--- 2022/rust/src/bin/day24/main.rs
+++ 2022/rust/src/bin/day24/main.rs
@@ -1,17 +1,38 @@
 use advent_of_code::{add_arrays, get_day, read_lines};
-use std::collections::{HashSet, VecDeque};
+use std::{
+    cmp::Ordering,
+    collections::{BinaryHeap, HashSet},
+};
+
+#[derive(Eq)]
+struct State {
+    f: u32,
+    time: u32,
+    position: [i32; 2],
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.f.cmp(&self.f)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for State {
+    fn eq(&self, other: &Self) -> bool {
+        self.f == other.f
+    }
+}
 
 #[derive(PartialEq, Eq, Hash, Copy, Clone)]
 struct Blizzard {
     position: [i32; 2],
     direction: [i32; 2],
-}
-
-impl Blizzard {
-    fn forward(&mut self, width: i32, height: i32) {
-        let new_pos = add_arrays(self.position, self.direction);
-        self.position = [new_pos[0].rem_euclid(width), new_pos[1].rem_euclid(height)];
-    }
 }
 
 fn main() {
@@ -44,46 +65,56 @@ fn solve(filename: &str) -> (u32, u32) {
         }
     }
 
-    let p1 = traverse(blizzards.clone(), width, height, [0, -1], [width - 1, height], 1);
-    let p2 = traverse(blizzards, width, height, [0, -1], [width - 1, height], 3);
+    let result = traverse(&blizzards, width, height, [0, -1], [width - 1, height], 0, 3);
 
-    return (p1, p2);
+    return (*result.first().unwrap(), *result.last().unwrap());
 }
 
-fn traverse(initial_blizzards: Vec<Blizzard>, width: i32, height: i32, start: [i32; 2], end: [i32; 2], remaining: usize) -> u32 {
+fn traverse(blizzards: &Vec<Blizzard>, width: i32, height: i32, start: [i32; 2], end: [i32; 2], time: u32, remaining: usize) -> Vec<u32> {
     if remaining == 0 {
-        return 0;
+        return Vec::new();
     }
 
-    let mut open = VecDeque::from([(initial_blizzards, start, 0)]);
+    let mut open = BinaryHeap::from([State { f: 0, position: start, time }]);
     let mut closed = HashSet::new();
 
-    loop {
-        let (mut blizzards, pos, time) = open.pop_front().unwrap();
-        if pos == end {
-            return time + traverse(blizzards, width, height, end, start, remaining - 1);
-        }
-        if !closed.insert((time, pos)) {
+    while let Some(State { f: _, time, position }) = open.pop() {
+        if !closed.insert((time, position)) {
             continue;
         }
 
         let mut valley: HashSet<[i32; 2]> = HashSet::new();
-        for blizzard in &mut blizzards {
-            blizzard.forward(width, height);
-            valley.insert(blizzard.position);
+        for blizzard in blizzards {
+            if (position[0] >= blizzard.position[0] - 1 && position[0] <= blizzard.position[0] + 1)
+                || (position[1] >= blizzard.position[1] - 1 && position[1] <= blizzard.position[1] + 1)
+            {
+                let blizzard_pos = [
+                    (blizzard.position[0] + blizzard.direction[0] * (time + 1) as i32).rem_euclid(width),
+                    (blizzard.position[1] + blizzard.direction[1] * (time + 1) as i32).rem_euclid(height),
+                ];
+                valley.insert(blizzard_pos);
+            }
         }
 
         for option in [[1, 0], [0, 1], [-1, 0], [0, -1], [0, 0]] {
-            let new_pos = add_arrays(pos, option);
+            let new_pos = add_arrays(position, option);
 
-            if (pos == start && new_pos == start)
-                || new_pos == end
-                || new_pos[0] >= 0 && new_pos[0] <= width - 1 && new_pos[1] >= 0 && new_pos[1] <= height - 1 && !valley.contains(&new_pos)
-            {
-                open.push_back((blizzards.clone(), new_pos, time + 1));
+            if new_pos == end {
+                let mut result = traverse(blizzards, width, height, end, start, time + 1, remaining - 1);
+                result.insert(0, time + 1);
+                return result;
+            }
+
+            if new_pos == start || new_pos[0] >= 0 && new_pos[0] <= width - 1 && new_pos[1] >= 0 && new_pos[1] <= height - 1 && !valley.contains(&new_pos) {
+                open.push(State {
+                    f: time + new_pos[0].abs_diff(end[0]) + new_pos[1].abs_diff(end[1]),
+                    position: new_pos,
+                    time: time + 1,
+                });
             }
         }
     }
+    unreachable!();
 }
 
 #[cfg(test)]
