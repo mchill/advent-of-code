ec55e43b200d21f30f802dd3448c0ec55ce4477a
Matthew Hill <matthew.hill@voith.com>
2024-12-02 16:05:12 -0500

    2024 Day 2
    Parsing files with Java is pretty bulky so far. Might need to
    write some helper methods soon.

---
+++ 2024/java/day2/Solution.java
@@ -0,0 +1,72 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        String input = readInput(args[0]);
+        System.out.println("Part 1: " + part1(input));
+        System.out.println("Part 2: " + part2(input));
+    }
+
+    private static String readInput(String path) throws IOException {
+        return Files.readString(Paths.get(path));
+    }
+
+    private static Integer part1(String input) {
+        Integer safeTotal = 0;
+
+        List<List<Integer>> reports = Arrays.stream(input.split("\n"))
+                .map(line -> Arrays.stream(line.split(" ")).map(Integer::valueOf).collect(Collectors.toList()))
+                .collect(Collectors.toList());
+
+        for (List<Integer> report : reports) {
+            List<List<Integer>> options = Arrays.asList(report, report.reversed());
+            safeTotal += options.stream().anyMatch(r -> isReportSafe(r)) ? 1 : 0;
+        }
+
+        return safeTotal;
+    }
+
+    private static Integer part2(String input) {
+        Integer safeTotal = 0;
+
+        List<List<Integer>> reports = Arrays.stream(input.split("\n"))
+                .map(line -> Arrays.stream(line.split(" ")).map(Integer::valueOf).collect(Collectors.toList()))
+                .collect(Collectors.toList());
+
+        for (List<Integer> report : reports) {
+            List<List<Integer>> options = new ArrayList<>();
+            options.add(report);
+            options.addAll(getDampenedReports(report));
+            options.add(report.reversed());
+            options.addAll(getDampenedReports(report.reversed()));
+            safeTotal += options.stream().anyMatch(r -> isReportSafe(r)) ? 1 : 0;
+        }
+
+        return safeTotal;
+    }
+
+    private static Boolean isReportSafe(List<Integer> report) {
+        for (int i = 1; i < report.size(); i++) {
+            if (report.get(i) <= report.get(i - 1) || report.get(i) > report.get(i - 1) + 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<List<Integer>> getDampenedReports(List<Integer> report) {
+        List<List<Integer>> reports = new ArrayList<List<Integer>>();
+        for (int i = 0; i < report.size(); i++) {
+            List<Integer> dampenedReport = new ArrayList<>(report);
+            dampenedReport.remove(i);
+            reports.add(dampenedReport);
+        }
+        return reports;
+    }
+}
