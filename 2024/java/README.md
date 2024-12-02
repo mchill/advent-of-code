66a0cfb0023ad9700e9fb30600a8430c6acb6d9b
Matthew Hill <matthew.hill@voith.com>
2024-12-01 23:05:44 -0500

    2024 Day 1
    10 years since the last time I seriously used Java, I'm back.

---
+++ 2024/java/README.md
@@ -0,0 +1,15 @@
+# Commands
+
+To run the solution for a given day against my input:
+
+```bash
+cd day#
+java Solution.java input.txt
+```
+
+To test the solution against the sample input:
+
+```bash
+cd day#
+java Solution.java sample.txt
+```
---
+++ 2024/java/day1/Solution.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        String input = readInput(args[0]);
+        System.out.println("Part 1: " + part1(input));
+        System.out.println("Part 2: " + part2(input));
+    }
+
+    private static String readInput(String path) throws IOException {
+        return Files.readString(Paths.get(path));
+    }
+
+    private static Integer part1(String input) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        for (String line : input.split("\n")) {
+            List<Integer> coordinates = Arrays.stream(line.split("\\s+"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            list1.add(coordinates.get(0));
+            list2.add(coordinates.get(1));
+        }
+
+        list1.sort(Integer::compareTo);
+        list2.sort(Integer::compareTo);
+
+        Integer diff = 0;
+
+        for (int i = 0; i < list1.size(); i++) {
+            diff += Math.abs(list1.get(i) - list2.get(i));
+        }
+
+        return diff;
+    }
+
+    private static Integer part2(String input) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        for (String line : input.split("\n")) {
+            List<Integer> coordinates = Arrays.stream(line.split("\\s+"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            list1.add(coordinates.get(0));
+            list2.add(coordinates.get(1));
+        }
+
+        Integer similarity = 0;
+
+        for (int number : list1) {
+            similarity += number * Collections.frequency(list2, number);
+        }
+
+        return similarity;
+    }
+}
